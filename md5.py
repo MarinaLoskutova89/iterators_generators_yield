c1883c59055eabd2b80cf3735ddd0d37a0f1d9b4
Marina <mloskutova1589@gmail.com>
2021-09-14 19:54:48 +0400

    MD5 corrected

--- md5.py
+++ md5.py
@@ -3,7 +3,7 @@ import hashlib
 def MD5_hash(string, encoding='utf-8'):
     md5_hasher = hashlib.md5()
     md5_hasher.update(string.encode(encoding))
-    return md5_hasher.hexdigest()
+    yield md5_hasher.hexdigest()
 
 with open('country_link.json') as file:
     for line in file:
